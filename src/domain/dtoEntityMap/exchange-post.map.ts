111f721d3a41e1df1f4dd7df4e1a027a8ed32227
SerhiiDiukarev <serhii.diukarev@telus.com>
2022-10-10 13:21:37 +0300

    refactoring

--- src/domain/dtoEntityMap/exchange-post.map.ts
+++ src/domain/dtoEntityMap/exchange-post.map.ts
@@ -6,56 +6,6 @@ import ExchangeGetEntity from '../entities/exchange-get.entity';
 import ExchangePostEntity, { NpaExchangePostEntity } from '../entities/exchange-post.entity';
 
 export class ExchangePostMap {
-    static dtoFieldToEntityFieldMapping: any = {
-        rn: 'rn',
-        abbrev: 'exchAbbrev',
-        bookNum: 'bookNum',
-        createdTs: 'createTs',
-        createdUserId: 'createUserId',
-        exchangeFullName: 'exchFullName',
-        lastUpdatedTs: 'lastUpdtTs',
-        lastUpdatedUserId: 'lastUpdtUserId',
-        secondAbbrev: 'exchAbbrev2',
-        sectionNum: 'sectionNum'
-    };
-
-    static entityToDto(entities: ExchangeGetEntity[]): ExchangeGetDto[] {
-        const dtos: ExchangeGetDto[] = [];
-        let currDto: ExchangeGetDto | undefined = undefined;
-        let currRn: number | undefined = undefined;
-
-        entities.forEach((entity) => {
-            const npaExchangeGetDto = new NpaExchangeGetDto();
-
-            if (!isNullOrUndefined(entity.bnemNpaExchId) && !isNullOrUndefined(entity.bnemNpa)) {
-                npaExchangeGetDto.bnemNpa = entity.bnemNpa;
-                npaExchangeGetDto.bnemNpaExchId = entity.bnemNpaExchId;
-            }
-
-            if (!currDto || currRn !== entity.rn) {
-                currRn = entity.rn;
-
-                currDto = new ExchangeGetDto();
-                currDto.abbrev = entity.exchAbbrev;
-                currDto.bookNum = entity.bookNum;
-                currDto.createdTs = entity.createTs;
-                currDto.createdUserId = entity.createUserId;
-                currDto.exchangeFullName = entity.exchFullName;
-                currDto.lastUpdatedTs = entity.lastUpdtTs;
-                currDto.lastUpdatedUserId = entity.lastUpdtUserId;
-                currDto.secondAbbrev = entity.exchAbbrev2;
-                currDto.sectionNum = entity.sectionNum;
-                currDto.npa = [];
-
-                dtos.push(currDto);
-            }
-            if (currDto.npa) {
-                currDto.npa.push(npaExchangeGetDto);
-            }
-        });
-        return dtos;
-    }
-
     static dtoToEntityForCreate(requestParam: RequestParam): ExchangePostEntity {
         const entity: ExchangePostEntity = new ExchangePostEntity();
         const dto: ExchangePostDto = requestParam.inputRequest;
@@ -78,38 +28,6 @@ export class ExchangePostMap {
             return npaEntity
         });
         return entity;
-    }
-
-    // static dtoToEntityForUpdate(requestParam: UserReportDetailRequestParam): HaaAssignedUserReportEntity {
-    //     const entity = new HaaAssignedUserReportEntity();
-    //     const dto: HaaUserReportDetailDto = requestParam.inputRequest;
-    //     dto.recipientUserId && (entity.recipientUserId = dto.recipientUserId);
-    //     dto.format && (entity.formatCode = dto.format);
-    //     dto.language && (entity.reportLanCode = dto.language);
-    //     return entity;
-    //   }
-
-    static mapDtoToEntitySortParams(sortParams: SortParam[]): SortParam[] {
-        if (!sortParams.length) {
-            sortParams = this.getDefaultSortParam();
-        }
-        const sortParamss: any[] = [];
-        for (const sortParam of sortParams) {
-            const entityField = this.dtoFieldToEntityFieldMapping[sortParam.fieldName];
-            if (entityField) {
-                sortParamss.push({ ...sortParam, fieldName: entityField });
-            }
-        }
-        return sortParamss;
-    }
-
-    static getDefaultSortParam(): SortParam[] {
-        return [
-            {
-                fieldName: 'exchangeFullName', // todo: what should be as a default?
-                asc: false,
-            },
-        ];
     }
 
 }
